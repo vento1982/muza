a071ff82219da29a0181757d087493bd5821ba42
vento <marcin.vento@gmail.com>
2016-10-18 23:12:27 +0200

    Add New and Create Action with View Files

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -4,18 +4,21 @@ class PlaysController < ApplicationController
 	end
 
 	def new
+		@play = Play.new
 	end
 
 	def create
+		@play = Play.new(play_params)
+		if @play.save
+			redirect_to root
+		else
+			render 'new'
+		end
 	end
 
-	def edit
-	end
+	private
 
-	def update
+	def play_params
+		params.require(:play).permit(:title, :description, :director)
 	end
-
-	def destroy
-	end
-	
 end
