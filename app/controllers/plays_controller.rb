f66f725ba9371066036e52afebfde52c1f256ed2
vento <marcin.vento@gmail.com>
2016-10-20 21:42:14 +0200

    Change Category Files on Index Page and index Action

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -2,7 +2,12 @@ class PlaysController < ApplicationController
 	before_action :find_play, only: [:show, :edit, :update, :destroy]
 
 	def index
-		@plays = Play.all.order("created_at DESC")
+		if params[:category].blank?
+			@plays = Play.all.order("created_at DESC")
+		else
+			@category_id = Category.find_by(name: params[:category]).id
+			@plays = Play.where(category_id: @category_id).order("created_at DESC")
+		end
 	end
 
 	def show
@@ -11,7 +16,6 @@ class PlaysController < ApplicationController
 	def new
 		@play = current_user.plays.build
 		@categories = Category.all.map{ |c| [c.name, c.id]}
-
 	end
 
 	def create
