5e592da5737492bccbe294eac1fead86f4ee77e4
vento <marcin.vento@gmail.com>
2016-10-26 22:34:36 +0200

    Add Average Raview and User Authentication

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -1,4 +1,5 @@
 class PlaysController < ApplicationController
+	before_action :authenticate_user!, only: [:new, :edit]
 	before_action :find_play, only: [:show, :edit, :update, :destroy]
 
 	def index
@@ -11,7 +12,11 @@ class PlaysController < ApplicationController
 	end
 
 	def show
-		
+		if @play.reviews.blank?
+			@average_review = 0
+		else
+			@average_review = @play.reviews.average(:rating).round(2)
+		end
 	end
 
 	def new
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,6 @@
 class ReviewsController < ApplicationController
 
+before_action :authenticate_user!, only: [:new, :edit]
 before_action :find_play
 before_action :find_review, only: [:edit, :update, :destroy]
 
